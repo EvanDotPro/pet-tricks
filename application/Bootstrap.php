d2871f5ccac9785e21b95513d63285552788c7c2
Evan Coury <me@evancoury.com>
2011-09-03 14:43:33 -0700

    fixed up autoloader for models/etc

---
+++ application/Bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+class Bootstrap
+    extends Zend_Application_Bootstrap_Bootstrap
+{
+    // This only exists because the ZF Bootstrap object uses reflection for the 
+    // module path to set up the autoloader.
+}
--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -3,6 +3,8 @@ class UserController extends Zend_Controller_Action
 {
     public function indexAction()
     {
+        $user = new Application_Model_User();
+        var_dump($user);
         $hybridauth = $this->getInvokeArg('bootstrap')->getPluginResource('hybridAuth')->getHybridAuth(); 
         $provider_adapter = $hybridauth->wakeup(); 
         $user_data        = $provider_adapter->user();
