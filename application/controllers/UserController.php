c62b1cdc9e0a601ea40647986ea6d902dc909e13
Evan Coury <me@evancoury.com>
2011-09-03 14:11:27 -0700

    initial schema with dog breeds imported from American Kennel Club

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -6,10 +6,6 @@ class UserController extends Zend_Controller_Action
         $hybridauth = $this->getInvokeArg('bootstrap')->getPluginResource('hybridAuth')->getHybridAuth(); 
         $provider_adapter = $hybridauth->wakeup(); 
         $user_data        = $provider_adapter->user();
-        var_dump($this->getRequest()->getRequestUri());
-        var_dump($this->getRequest()->getBaseUrl());
-        print_r($user_data); 
-        die();
     }
 }
 
